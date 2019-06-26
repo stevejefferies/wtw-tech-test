d0613c2a452e2413cb199b53a1824ae39bf7421f
Steve Jefferies <stevejeffers@gmail.com>
2019-06-26 19:06:23 +0100

    Tidy test spec and utilise region check logic

--- specs/search-test-spec.js
+++ specs/search-test-spec.js
@@ -1,9 +1,8 @@
-// Load dependecies
-var assert = require('chai').assert,
 test = require('selenium-webdriver/testing'),
 webdriver = require('selenium-webdriver'),
 until = webdriver.until,
-By = webdriver.By,
+By = webdriver.By;
+
 HomePage = require('../lib/home.po');
 SearchPage = require('../lib/search.po');
 
@@ -18,24 +17,21 @@ test.describe('Test', function () {
     withCapabilities(webdriver.Capabilities.chrome()).
     build();
 
-    // add cookies to cover GDPR & region settings
+    // add cookie to cover GDPR settings
     driver.get('https://www.willistowerswatson.com');
     driver.manage().addCookie({ 'name': 'notice_gdpr_prefs', 'value': '0,1,2:', 'path': '/', 'domain': '.willistowerswatson.com' })
-    driver.manage().addCookie({ 'name': 'wtw#lang', 'value': 'en', 'path': '/', 'domain': '.willistowerswatson.com' })
-
   });
 
   test.afterEach(function() {
-    // Quit webdriver
     driver.quit();
   })
 
   test.it('Can search via WTW homepage"', function () {
     var homePage = new HomePage(driver);
     homePage.get();
+    homePage.ensureRegion('Global | English');
 
-    homePage.search('test')
-
+    homePage.search('test');
     // check results page loaded
     driver.wait(until.titleIs('Search - Willis Towers Watson'), 1000);
 
